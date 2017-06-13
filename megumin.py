697d97070c5e60bb8effd51d3266b7b278f30311
Muttsu <22158218+Muttsu@users.noreply.github.com>
2017-06-13 04:31:35 -0400

    changed how the command is parsed

--- megumin.py
+++ megumin.py
@@ -15,11 +15,22 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
-    if client.user in message.mentions and 'ping' in message.content:
-        ping = message.timestamp
-        pong = datetime.now()
-        latency = (pong - ping).microseconds // 1000
-        await client.send_message(message.channel,'pong - ' + str(latency))
-        print('ping')
+
+    if client.user in message.mentions:
+        words = message.content.split
+        words.remove('<@' + str('client.user.id') + '>')
+        command = ' '.join(words)
+    elif message.content.startwith('.'):
+        command = message.content[1:]
+    else:
+        command = None
+
+    if command != None
+        if client.user in message.mentions and 'ping' in message.content:
+            ping = message.timestamp
+            pong = datetime.now()
+            latency = (pong - ping).microseconds // 1000
+            await client.send_message(message.channel,'pong - ' + str(latency))
+            print('ping')
         
 client.run('MzIyMTg2OTkwNTc5NzQ0Nzcy.DCD7VA.yIQEIeyd3QZrhzVVBw6Nguyihx4')
