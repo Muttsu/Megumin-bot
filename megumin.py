16514d2a3ef945cd180efb5c8099d7f303a1dda7
Muttsu <22158218+Muttsu@users.noreply.github.com>
2017-06-13 06:47:34 -0400

    changed to parsecommand

--- megumin.py
+++ megumin.py
@@ -17,20 +17,17 @@ async def on_message(message):
         return
 
     if message.content.startswith('.'):
-        command = message.content[1:].split()
+        pasecommand(message.content[1:].split())
     elif message.content.startswith('<@!{}>'.format(client.user.id)):
         query = message.content.split()
-        command = query.remove('<@!{}>'.format(client.user.id))
-    else:
-        command = None
+        parsecommand(query.remove('<@!{}>'.format(client.user.id)))
 
-    if command != None:
-        #parsecommand(command)
-        if command == 'ping':
-            ping = message.timestamp
-            pong = datetime.now()
-            latency = (pong - ping).microseconds // 1000
-            await client.send_message(message.channel,'pong - ' + str(latency))
-            print('ping')
+def parsecommand(command):
+    if command[0] == 'ping':
+        a = message.timestamp
+        b = datetime.now()
+        latency = (b - a).microseconds // 1000
+        await client.send_message(message.channel,'pong - ' + str(latency))
+        print('ping')
         
 client.run('MzIyMTg2OTkwNTc5NzQ0Nzcy.DCD7VA.yIQEIeyd3QZrhzVVBw6Nguyihx4')
