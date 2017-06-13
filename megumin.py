83bfaeb002424e4b4f4dd5f82d182d1b3cd15aa0
Muttsu <22158218+Muttsu@users.noreply.github.com>
2017-06-13 09:47:29 -0400

    fixed passing context

--- megumin.py
+++ megumin.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 import asyncio
 from datetime import datetime
 
-bot = commands.Bot()
+bot = commands.Bot(command_prefix = '?')
 
 @bot.event
 async def on_ready():
@@ -13,12 +13,12 @@ async def on_ready():
     bot.command_prefix = ['?','<@!{0}'.format(bot.user.id)]
     print(':3')
 
-@bot.command()
-async def ping():
-    a = message.timestamp
+@bot.command(pass_context = True)
+async def ping(ctx):
+    a = ctx.message.timestamp
     b = datetime.now()
     latency = (b - a).microseconds // 1000
-    bot.send_message(message.channel,'pong - ' + str(latency))
+    bot.send_message(ctx.message.channel,'pong - ' + str(latency))
     print('ping')
         
 bot.run('MzIyMTg2OTkwNTc5NzQ0Nzcy.DCD7VA.yIQEIeyd3QZrhzVVBw6Nguyihx4')
