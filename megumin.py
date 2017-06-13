879e48f6fd7e5bcdcca58932b1eb0a99ed6dfc83
Muttsu <22158218+Muttsu@users.noreply.github.com>
2017-06-13 02:30:38 -0400

    added test bot
    ping pong

---
+++ megumin.py
@@ -0,0 +1,21 @@
+import discord
+import asyncio
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print()
+    
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('.ping'):
+        await client.send_message(message.channel,'pong')
+        
+client.run('MzIyMTg2OTkwNTc5NzQ0Nzcy.DCD7VA.yIQEIeyd3QZrhzVVBw6Nguyihx4')
+        
