bdd17becf797690c44bc2569aba8fd4c3deb8741
Muttsu <22158218+Muttsu@users.noreply.github.com>
2017-06-13 09:33:38 -0400

    changed to use ext.commands

--- megumin.py
+++ megumin.py
@@ -1,35 +1,24 @@
 import discord
+from discord.ext import commands
 import asyncio
 from datetime import datetime
 
-client = discord.Client()
+bot = commands.Bot()
 
-@client.event
+@bot.event
 async def on_ready():
     print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
+    print(bot.user.name)
+    print(bot.user.id)
+    bot.command_prefix = ['?','<@!{0}'.format(bot.user.id)]
     print(':3')
-    
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
 
-    if message.content.startswith('.'):
-        pasecommand(message.content[1:],message)
-    elif message.content.startswith('<@!{}>'.format(client.user.id)):
-        query = message.content.split()
-        result = query.remove('<@!{}>'.format(client.user.id))
-        command = ' '.join(result)
-        parsecommand(command,message)
-
-def parsecommand(command,message):
-    if command == 'ping':
-        a = message.timestamp
-        b = datetime.now()
-        latency = (b - a).microseconds // 1000
-        client.send_message(message.channel,'pong - ' + str(latency))
-        print('ping')
+@bot.command()
+async def ping():
+    a = message.timestamp
+    b = datetime.now()
+    latency = (b - a).microseconds // 1000
+    bot.send_message(message.channel,'pong - ' + str(latency))
+    print('ping')
         
-client.run('MzIyMTg2OTkwNTc5NzQ0Nzcy.DCD7VA.yIQEIeyd3QZrhzVVBw6Nguyihx4')
+bot.run('MzIyMTg2OTkwNTc5NzQ0Nzcy.DCD7VA.yIQEIeyd3QZrhzVVBw6Nguyihx4')
