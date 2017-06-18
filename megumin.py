82bf7cd21eb072350ebc406d4b0f355c5c6b8194
Muttsu <22158218+Muttsu@users.noreply.github.com>
2017-06-18 10:58:39 -0400

    Updated Syntax

--- megumin.py
+++ megumin.py
@@ -3,33 +3,48 @@ from discord.ext import commands
 import asyncio
 from datetime import datetime
 
-bot = commands.Bot(command_prefix = '?')
+bot = commands.Bot(command_prefix='?')
+
 
 @bot.event
 async def on_ready():
     print('Logged in as')
     print(bot.user.name)
     print(bot.user.id)
-    bot.command_prefix = ['?','<@!{0}> '.format(bot.user.id), '<@{0}> '.format(bot.user.id)]
+    bot.command_prefix = ['?',
+                          '<@!{0}> '.format(bot.user.id),
+                          '<@{0}> '.format(bot.user.id)]
     print(':3')
+
+
+# Ping {{{
 
 async def ping_check(message):
     a = message.timestamp
     b = datetime.now()
     latency = (b - a).microseconds // 1000
-    pong = await bot.send_message(message.channel, 'pong    {0}ms'.format(str(latency)))
+    pong = await bot.send_message(message.channel,
+                                  'pong    {0}ms'.format(str(latency)))
     c = pong.timestamp
     latency2 = (c - b).microseconds // 1000
-    await bot.edit_message(pong,pong.content+'    ping    {0}ms'.format(str(latency2)))
-    print('ping from {0}    {1}    {2}    ms'.format(str(message.author), str(latency), str(latency2)))
+    await bot.edit_message(pong,
+                           '{1}    ping    {0}ms'.format(str(latency2),
+                                                         pong.content))
+    print('ping from {0}    {1}    {2}    ms'.format(str(message.author),
+                                                     str(latency),
+                                                     str(latency2)))
 
-@bot.command(pass_context = True)
+
+@bot.command(pass_context=True)
 async def ping(ctx):
     await ping_check(ctx.message)
+
 
 @bot.listen()
 async def on_message(message):
     if message.content == 'ping':
         await ping_check(message)
+
+# }}}
 
 bot.run('MzIyMTg2OTkwNTc5NzQ0Nzcy.DCD7VA.yIQEIeyd3QZrhzVVBw6Nguyihx4')
