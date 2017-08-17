c1480a04fafa5c6ab1000fd33930e6d095ca713a
i3wgnit <i3wgnit@users.noreply.github.com>
2017-08-16 22:20:36 -0400

    Update megumin.py

--- megumin.py
+++ megumin.py
@@ -27,17 +27,39 @@ async def ping_check(message):
     await bot.edit_message(pong,'{0}    pong({1}ms)'.format(pong.content, str(pong_latency)))
     print('ping from {0}    {1}    {2}    ms'.format(str(message.author), str(ping_latency), str(pong_latency)))
 
+# }}}
+# Spam Function
 
-@bot.command(pass_context=True)
-async def ping(ctx):
-    await ping_check(ctx.message)
+async def spam_function(message):
+    def log_error(error):
+        bot.edit_message(header,
+            """```
+            -- Spam Function Error:[{}] --
+            ```""".format(error))
 
+    header = await bot.send_message(message.channel, "```\n-- Spam Function Active --\n```")
+    args = message.content.split()
+    if len(args) < 3:
+        log_error("2 arguments required")
+    else:
+        await bot.delete_message(message)
+        tmp = []
+        reps = int(args[1]) or 3
+        for _ in range(reps):
+            tmp.append(await bot.send_message(message.channel, "{} - spam {}".format(" ".join(args[2:]), _ + 1)))
+        for msg in tmp[::-1]:
+            await bot.delete_message(msg)
+        await bot.delete_message(header)
+
+# }}}
 
 @bot.listen()
 async def on_message(message):
     if message.content == 'ping':
         await ping_check(message)
-
-# }}}
+    elif message.content.startswith('spam '):
+        await spam_function(message)
+    elif message.content == "§die":
+        exit()
 
 bot.run('MzIyMTg2OTkwNTc5NzQ0Nzcy.DCD7VA.yIQEIeyd3QZrhzVVBw6Nguyihx4')
