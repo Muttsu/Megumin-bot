283345fb977e3eb6e0383f56823a534db0ea8c1d
i3wgnit <i3wgnit@users.noreply.github.com>
2017-08-17 22:19:57 -0400

    Update megumin.py

--- megumin.py
+++ megumin.py
@@ -1,6 +1,8 @@
 import discord
 from discord.ext import commands
 import asyncio
+
+import re
 from datetime import datetime
 
 bot = commands.Bot(command_prefix='?')
@@ -12,8 +14,8 @@ async def on_ready():
     print(bot.user.name)
     print(bot.user.id)
     bot.command_prefix = ['?',
-        '<@!{0}> '.format(bot.user.id),
-        '<@{0}> '.format(bot.user.id)]
+        '<@!{}> '.format(bot.user.id),
+        '<@{}> '.format(bot.user.id)]
 
 
 # Ping {{{
@@ -22,10 +24,10 @@ async def ping_check(message):
     ping = message
     local_time = datetime.now()
     ping_latency = (local_time - ping.timestamp).microseconds // 1000
-    pong = await bot.send_message(message.channel, 'ping({0}ms)'.format(str(ping_latency)))
+    pong = await bot.send_message(message.channel, 'ping({}ms)'.format(str(ping_latency)))
     pong_latency = (pong.timestamp - local_time).microseconds // 1000
-    await bot.edit_message(pong,'{0}    pong({1}ms)'.format(pong.content, str(pong_latency)))
-    print('ping from {0}    {1}    {2}    ms'.format(str(message.author), str(ping_latency), str(pong_latency)))
+    await bot.edit_message(pong,'{} pong({}ms)'.format(pong.content, str(pong_latency)))
+    print('{}: ping({}ms) pong({}ms)'.format(message.author, ping_latency, pong_latency))
 
 # }}}
 # Spam Function {{{
@@ -34,8 +36,8 @@ async def spam_function(message):
     async def log_error(error):
         await bot.edit_message(header,
             """```
-            -- Spam Function Error:[{}] --
-            ```""".format(error))
+-- Spam Function Error:[{}] --
+```""".format(error))
 
     header = await bot.send_message(message.channel, "```\n-- Spam Function Active --\n```")
     args = message.content.split()
@@ -51,35 +53,37 @@ async def spam_function(message):
             await bot.delete_message(msg)
         await bot.delete_message(header)
 
+# }}}
 
 @bot.command(pass_context=True)
 async def spam(ctx, count : int, *, content):
+    print("{}: Spam [{}, \"{}\"]".format(ctx.message.author, count, content))
+
+    if(ctx.message.channel.name != "megumin-test"):
+        await bot.delete_message(ctx.message)
+
     async def editCount(index):
         await bot.edit_message(header,
             "```\n-- Spam Function Active [{}] --\n```".format(index))
 
     header = await bot.say("```\n-- Spam Function Active --\n```")
-    await bot.delete_message(ctx.message)
     await bot.say(content)
     for _ in range(count):
         await bot.delete_message(await bot.say(content))
         await editCount(_+1)
     await bot.delete_message(header)
-
-# }}}
-# Explosion {{{
-
+    
 @bot.command(pass_context=True)
 async def explosion(ctx, id, count=3, *, content="EXPLOSION"):
-    await bot.delete_message(ctx.message)
+    print("{}: explosion [{}, {}, \"{}\"]".format(ctx.message.author, member.name, count, content))
 
-	author = ctx.message.author
+    if(ctx.message.channel.name != "megumin-test"):
+        await bot.delete_message(ctx.message)
+
+    author = ctx.message.author
     member = discord.utils.get(bot.get_all_members(), id=re.sub("\D+", "", id))
     for _ in range(count):
         await bot.send_message(member, "<@!{}> -> <@!{}>: {}".format(author.id, member.id, content))
-    print("{}: explosion [{}, {}, \"{}\"]".format(ctx.message.author, member.name, count, content))
-
-#}}}
 
 @bot.listen()
 async def on_message(message):
