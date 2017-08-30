a3df1fa53befb0c99b1c93835327f36629db7c7a
i3wgnit <i3wgnit@users.noreply.github.com>
2017-08-29 21:58:53 -0400

    stylish style for futur self

--- core.py
+++ core.py
@@ -1,4 +1,6 @@
 import asyncio
+import json
+from datetime import datetime
 
 commands = {}
 
@@ -6,24 +8,24 @@ def command(name=None, **kwargs):
     def wrapper(fn):
         # add the command to dict
         nm = name or fn.__name__
-        commands[nm] = Command(name = nm, func = fn, **kwargs)
+        commands[nm] = Command(func = fn, **kwargs)
 
         return fn
     return wrapper
 
 class Command:
     def __init__(self, *args, **kwargs):
-        self.name = kwargs.pop("name", None)
         self.func = kwargs.pop("func", None)
+        self.doc = self.func.__doc__
         self.ctx = kwargs.pop("ctx", None)
         self.ignore_ctx = kwargs.pop("ignore_ctx", False)
-    
+
     async def __call__(self, *args, **kwargs):
         if self.ignore_ctx:
             return await self.func(*args, **kwargs)
         else:
             return await self.func(self.ctx, *args, **kwargs)
-    
+
 class Context:
     def __init__(self, **kwargs):
         self.message = kwargs.pop("message", None)
@@ -31,4 +33,84 @@ class Context:
     
     async def say(self, msg):
         return await self.bot.send_message(self.message.channel, msg)
+
+
+# == New Section ==
+with open("config.json", "r") as f:
+    aliases = json.load(f)["aliases"]
+    f.close()
+
+async def parse_message(bot, message):
+    """Transforms message content into an array of commands"""
+
+    content = message.content
+    for pfx in bot.prefix:
+        if content.startswith(pfx):
+            # Remove prefix from message
+            commands = map(lambda string: string.strip(),
+                # Split the commands
+                content.replace(pfx, "", 1).split(" & "))
+            break
+
+    for cmd in commands:
+            await parse_command(cmd, bot, message)
+
+
+async def parse_command(command, bot, message):
+    """Parse individual commands"""
+
+    cmd = command.split(" ", 1)
+    func = parse_alias(cmd[0])
+    args = cmd[1:]
+
     
+
+    # Check if the command actually exists
+    if isinstance(func, Command):
+        # Smartz way to pass bot and message objects
+        func.ctx = Context(bot = bot, message = message)
+        try:
+            # Log the return value
+            log(message.author, "SUCCESS", command,
+                await func(*args))
+
+        # Error parsing
+        except Exception as e:
+            log(message.author, "ERROR", command, e)
+            await bot.send_message(message.channel, str(e))
+
+            # So I don't get depressed (◕‿◕✿)
+            if message.author.id in admin_ids:
+                await bot.send_message(message.channel, "Aww")
+            else:
+                await bot.send_message(message.channel, "Something went wrong and you're the cause. You suck.")
+    else:
+        log(message.author, "ERROR", command, "'{}': not a command".format(func))
+        
+def parse_alias(func_name):
+    # Alias of an Alias
+    if func_name in aliases:
+        return parse_alias(aliases[func_name])
+    # We found the Function
+    elif func_name in commands:
+        return commands[func_name]
+    else:
+        return func_name
+
+
+def log(author, state: str, message, info = None):
+    """Logging, Logging, Error Debugging"""
+    print("[{}] {:<20s}:{:<10s} {}".format(datetime.now().strftime("%H:%M:%S"), str(author), state, str(message)))
+    if info:
+        print("  > " + str(info))
+
+class Start():
+    def __init__(self):
+        print("Made by Muttsu and twl")
+        self.state = False
+    
+    def __nonzero__(self):
+        return self.state
+    
+    def __call__(self):
+        self.state = True
--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import asyncio
 import discord
 
-from core import commands, Context
+from core import *
 
 from datetime import datetime
 import json
 import importlib
 
-print("Strarting BOT...")
+
+ready = Start()
 
 bot = discord.Client()
 
@@ -21,24 +22,20 @@ token = secret["token"]
 admin_ids = secret["admins"]
 prefix = secret["prefix"]
 modules = secret["modules"]
+for mod in modules:
+    importlib.import_module("modules." + mod)
+print("Availiable Commands: {}".format(tuple(sorted(commands.keys()))))
 
-
-@bot.event
-async def on_message(message):
-    """Commands and Stuff"""
-
-    # Test Only
-    if message.channel.name == "megumin-test" and message.author != bot.user:
-
-        if message.content.startswith(tuple(prefix)):
-            await parse_message(message)
 
 
 @bot.event
 async def on_ready():
     """Are you ready?"""
 
-    print('Logged in as', bot.user.name, bot.user.id)
+    log(bot.user, "Logged in as", bot.user.id)
+    print("-"*63)
+
+    ready()
 
     # Prefix Stuff
     if isinstance(prefix, str):
@@ -50,66 +47,17 @@ async def on_ready():
     bot.prefix.append('<@!{}> '.format(bot.user.id))
 
 
-##################################
-# == Thats All You Need To Know ==
-##################################
+@bot.event
+async def on_message(message):
+    """Commands and Stuff"""
 
+    if ready:
+        # Test Only
+        if message.channel.name == "megumin-test" and message.author != bot.user:
+            if message.content == "§die":
+                exit()
+            if message.content.startswith(tuple(prefix)):
+                await parse_message(bot, message)
 
-async def parse_message(message):
-    """Transforms message content into an array of commands"""
-
-    content = message.content
-    for pfx in bot.prefix:
-        if content.startswith(pfx):
-            # Remove prefix from message
-            commands = map(lambda string: string.strip(),
-                # Split the commands
-                content.replace(pfx, "", 1).split(" & "))
-            break
-
-    for cmd in commands:
-            await parse_command(message, cmd)
-
-
-async def parse_command(message, command):
-    """Parse individual commands"""
-
-    cmd = command.split(" ", 1)
-    func_name = cmd[0]
-    args = cmd[1:]
-
-
-    # Check if the command actually exists
-    if func_name in commands:
-        func = commands[func_name]
-        # Smartz way to pass bot and message objects
-        func.ctx = Context(bot = bot, message = message)
-        try:
-            # Log the return value
-            log(message.author, "SUCCESS", func_name,
-                await func(*args))
-
-        # Error parsing
-        except Exception as e:
-            log(message.author, "ERROR", command, e)
-
-            # So I don't get depressed (◕‿◕✿)
-            if message.author.id in admin_ids:
-                await bot.send_message(message.channel, "Aww")
-            else:
-                await bot.send_message(message.channel, "Something went wrong and you're the cause. You suck.")
-    else:
-        log(message.author, "ERROR", command, "{}: not a command".format(func_name))
-
-def log(author, state: str, message = None, info = None):
-    """Logging, Logging, Error Debugging"""
-    print("[{}] {:<20s}:{:<10s} {}".format(datetime.now().strftime("%H:%M:%S"), str(author), state, str(message)))
-    if message:
-        print("  > " + str(info))
-
-
-for mod in modules:
-    importlib.import_module("modules." + mod)
-print("Availiable Commands: {}".format(tuple(commands.keys())))
 
 bot.run(token)
