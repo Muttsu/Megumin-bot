c0b6b09cd6d901032b5fe35e0d1ae38d8864ad37
i3wgnit <i3wgnit@gmail.com>
2017-08-31 16:38:57 -0400

    it works!

--- modules/utils.py
+++ modules/utils.py
@@ -7,7 +7,7 @@ from core import *
 async def help(ctx, func: str):
     """Displays the reference manual for commands and aliases
     Usage: help <func:str>"""
-    
+
     if func in aliases:
         doc = "'{}' is an alias for '{}'".format(func, aliases[func])
 
@@ -35,28 +35,31 @@ async def delMsg(ctx, *args, **kwargs):
     except:
         raise FunctionException()
 
-@command(ignore_carry = True, key_aliases = {"s": "silent"})
-async def ping(ctx, msg, silent = False):
+@command(ignore_carry = True, ignore_kwargs = True)
+async def ping(ctx, msg):
     """Returns the latency between the Server and the Bot
     Usage: ping [-s or -Silent]
     Return: ping"""
 
     local_time = datetime.now()
     ping_latency = (local_time - ctx.message.timestamp).microseconds // 1000
-    if not silent:
-        pong = await ctx.say('ping({}ms)'.format(str(ping_latency)))
+
+    pong = await ctx.say('ping({}ms)'.format(str(ping_latency)))
+    if pong is not None:
         pong_latency = (pong.timestamp - local_time).microseconds // 1000
         await ctx.bot.edit_message(pong, '{} pong({}ms)'.format(pong.content, str(pong_latency)))
+
     return ping_latency
 
 @command(key_aliases = {"carry": "formatstr", "s": "silent", "r": "raw"})
-async def echo(ctx, message: str, silent = False, raw = False, formatstr = ""):
+async def echo(ctx, message, raw = False, formatstr = ""):
     """Displays text [-r or -raw] [-s or -silent] [-formatstr:str]
     Usage: echo <msg:str>"""
-    
-    message = message.format(formatstr)
+
+    message = str(message).format(formatstr)
+
     if not raw:
         message = re.sub("\s+", " ", message).strip()
-    if not silent:
-        await ctx.say(message)
+    await ctx.say(message)
     return message
+
