97baf32127c426e000366f5c416ff17896a3e43f
Muttsu <22158218+Muttsu@users.noreply.github.com>
2017-12-29 23:59:51 -0500

    some work done on dscio

--- dscio.py
+++ dscio.py
@@ -1,7 +1,7 @@
 """io flow for discord messages"""
 import asyncio
 
-class Dscout():
+class dscout():
     """unbuffered output stream
     enables channel redirrection using a dict mapping"""
     def __init__(self, bot, pipes={}):
@@ -13,41 +13,78 @@ class Dscout():
         if not s:
             return # what are you returning???
             # need to implement exception
-        ch = asyncio.Task.current_task().ctx.invoker.channel
-        if ch in self.pipes:
-            self.pipes[ch].write(s)
+        channel = asyncio.Task.current_task().ctx.invoker.channel
+        if channel in self.pipes:
+            self.pipes[channel].write(s)
         else:
-            asyncio.ensure_future(self.bot.send_message(ch, s))
+            asyncio.ensure_future(self.bot.send_message(channel, s))
 
-    def flush(self)
-        raise #flush is not allowed
+    def new_pipe(self, from_ch, to_ch):
+        self.pipes[from_ch] = to_ch
+
+    def del_pipe(self, from_ch):
+        del self.pipes[from_ch]
+
+    def del_pipe_to(self, to_ch):
+        for k, v in self.pipes:
+            if v == to_ch:
+                del self.pipes[k]
 
 
-class Dscin(metaclass=Singleton):
+    def flush(self):
+        return #flush is not allowed
+
+
+class dscin():
     """buffered input stream
     enables channel redirrection using a dict mapping"""
     def __init__(self, bot, pipes={}):
-        self._stream = {}
+        self.stream = {}
         self.bot = bot
         self.pipes = pipes
 
     async def read(self, n=1):
         # todo raise exception if channel not in self.buffer
-        ch = asyncio.Task.current_task().ctx.invoker.channel
+        # todo read n times
+        channel = asyncio.Task.current_task().ctx.invoker.channel
         # only allows current channel
-        for i, msg in enumerate(stream)
+        if self.stream and channel in self.stream:
+            buf = self.stream[channel]
+            ret = await buf.get()
+            if buf.isempty():
+                del self.stream[channel]
+        else:
+            self.new_buffer(channel)
+            self.read(n)
 
     def write(self, s, channel=asyncio.Task.current_task().ctx.invoker.channel):
         # todo raise exception if channel not in self.buffer
-        asyncio.ensure_future(self.stream[channel].put(Dummy_msg(s, channel)))
+        # todo pipes
+        if self.stream and channel in self.pipes:
+            self.pipes[channel].write(s)
+        else:
+            if channel in self.stream:
+                asyncio.ensure_future(self.stream[channel].put(dummy_msg(s, channel)))
+            else:
+                self.new_buffer(channel)
 
     def new_buffer(self, channel=asyncio.Task.current_task().ctx.invoker.channel):
         # todo raise exception if channel in self.buffer
-        q = asyncio.Queue()
-        self.stream[channel] = q
+        self.stream[channel] = buffer()
 
     def close_buffer(self, channel=asyncio.Task.current_task().ctx.invoker.channel):
         del self.stream[channel]
+
+    def new_pipe(self, from_ch, to_ch):
+        self.pipes[from_ch] = to_ch
+
+    def del_pipe(self, from_ch):
+        del self.pipes[from_ch]
+
+    def del_pipe_to(self, to_ch):
+        for k, v in self.pipes:
+            if v == to_ch:
+                del self.pipes[k]
 
 class buffer():
     """underlaying buffer for dscin.
@@ -64,14 +101,17 @@ class buffer():
 
     async def get(self):
         with await self._lock:
-            size += 1
+            self.size += 1
             return await self._cargo.get()
 
 
     def put(self, element):
         self._cargo.put_nowait(element)
-        size -= 1
+        self.size -= 1
 
+
+    def isempty(self):
+        return self.size == 0
 
     async def _acquire(self):
         if not self._lock._locked and all(w.cancelled() for w in self._lock._waiters):
@@ -98,16 +138,19 @@ class buffer():
     async def __aenter__(self):
         return self._acquire()
 
-    async def __aexit__(self):
+    async def __aexit__(self, exc_type, exc, tb):
         self._release()
 
 
-    def flush():
+    def flush(self):
         del self._cargo
-        self._cargo = collections.deque()
+        self._cargo = asyncio.Queue()
 
 
 class dummy_msg():
     def __init__(self, content, channel=asyncio.Task.current_task().ctx.invoker.channel):
         self.channel = channel
         self.content = content
+
+dscin = dscin()
+dscout = dscout()
